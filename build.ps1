30841aab5cd0087ae0735e2db9a9ee3c83dfb5d0
Oscar Calvo <oscar@calvonet.com>
2020-07-02 15:12:19 -0700

    Add usage to script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 param(
-  $project = "WinForms",
+  $project,
   $Configuration="Release",
   $Platform = "x64",
   $BuildAction="ReBuild",
@@ -7,10 +7,17 @@ param(
   $Appx_Bundle_Platforms="x64|x86",
   $Appx_Package_Build_Mode="StoreUpload",
   $AppInstallerUri="https://msdn.microsoft.com",
+  $RunTimeIdentifiers="`"win-x64`;win-x86`;win10-x64-aot`;win10-x86-aot`"",
   [switch]$onlyRestore
 )
 
-if ($project -eq "WinForms")
+if ($null -eq $project)
+{
+  Write-Host "Usage:"
+  Write-Host "   >.\build.ps1 [WinForms|WPF|Native] [-Platform [x86|x64]] [-Configuration [Debug|Release]]"
+  return
+}
+elseif ($project -eq "WinForms")
 {
   $Solution_Name = "WinForms_XAMLIslands_v1.sln"
   $Wap_Project_Path="Samples\WinForms\WAP\WAP.wapproj"
@@ -33,7 +40,7 @@ else
 
 function build-internal()
 {
-  msbuild $Solution_Name /bl:logfile=restore.$project.$Configuration.$Platform.binlog /t:Restore /p:Configuration=$Configuration /p:Platform=$Platform /p:RuntimeIdentifiers=`"win-x64`;win-x86`;win10-x64-aot`;win10-x86-aot`"
+  msbuild $Solution_Name /bl:logfile=restore.$project.$Configuration.$Platform.binlog /t:Restore /p:Configuration=$Configuration /p:Platform=$Platform /p:RuntimeIdentifiers=$RunTimeIdentifiers
   if (!$onlyRestore.IsPresent)
   {
     $objFolder = ".\Samples\Win32\SampleCppApp\obj\"
@@ -41,7 +48,7 @@ function build-internal()
     {
       rmdir $objFolder -Force -Recurse
     }
-    msbuild $Wap_Project_Path /bl:logfile=$project.$Configuration.$Platform.binlog /t:$BuildAction /p:Configuration=$Configuration /p:Platform=$Platform /p:UapAppxPackageBuildMode=$Appx_Package_Build_Mode /p:AppxBundle=$Appx_Bundle /p:AppInstallerUri=$AppInstallerUri /p:RuntimeIdentifiers=`"win-x64`;win-x86`;win10-x64-aot`;win10-x86-aot`"
+    msbuild $Wap_Project_Path /bl:logfile=$project.$Configuration.$Platform.binlog /t:$BuildAction /p:Configuration=$Configuration /p:Platform=$Platform /p:UapAppxPackageBuildMode=$Appx_Package_Build_Mode /p:AppxBundle=$Appx_Bundle /p:AppInstallerUri=$AppInstallerUri /p:RuntimeIdentifiers=$RunTimeIdentifiers
   }
 }
 
