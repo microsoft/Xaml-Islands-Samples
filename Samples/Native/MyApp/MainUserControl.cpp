83202c71cd35c8a719638bd9218b6d2b326b62de
Stefan Markovic <stefan@janeasystems.com>
2021-09-29 18:53:36 +0200

    Test TreeView

--- Samples/Native/MyApp/MainUserControl.cpp
+++ Samples/Native/MyApp/MainUserControl.cpp
@@ -1,5 +1,6 @@
 ﻿#include "pch.h"
 #include "MainUserControl.h"
+#include <TreeViewNodeViewModel.h>
 
 using namespace winrt;
 using namespace Windows::UI::Xaml;
@@ -10,15 +11,21 @@ namespace winrt::MyApp::implementation
     {  
 
         InitializeComponent();
+        for (int i = 0; i < 800; i++) {
+            auto child = winrt::make<MyApp::implementation::TreeViewNodeViewModel>(L"Label", winrt::Windows::Foundation::Uri{ nullptr }, L"");
+
+            treeView().Nodes().Append(child);
+        }
     }
 
     winrt::hstring MainUserControl::MyProperty()
     {
-        return userControl().MyProperty();
+        return hstring{};
+        // return userControl().MyProperty();
     }
 
     void MainUserControl::MyProperty(winrt::hstring value)
     {
-        userControl().MyProperty(value);
+        // userControl().MyProperty(value);
     }
 }
